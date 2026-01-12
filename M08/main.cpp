325cdf3b1ae8f7f9d15a44c3fc07daea36d4b961
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2026-01-12 17:02:09 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   M08/main.cpp
    
    Untracked files:
    	M01/main
    	M02/main
    	M03/main
    	M04/main
    	M05/FavoriteSequence
    	M08/main
    	M08/tempCodeRunnerFile.cpp
    	README.md

---
+++ M08/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// <RETURN TYPE> <FUNC NAME>(<PARAM TYPE> <PARAM NAME>, ...) {
+//  CODE 
+//  return <VALUE>;
+// }
+void say_name(string name) {
+    cout << "Hello " << name << endl;
+}
+
+void print_array(int arr[], int size){
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+}
+
+bool is_even(int number) {
+    if (number % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+int main() {
+    int num;
+    cin >> num;
+    bool result = is_even(num);
+    bool result2 = is_even(10);
+    
+    if (result == true) {
+        cout << num << " is even" << endl;
+    } else {
+        cout << num << " is odd" << endl;
+    }
+    // <function name> (<argument>)
+    // say_name("Marvel");
+    // string d;
+    // cin >> d;
+    // say_name(d);
+
+    // int arr_main[] = {1,23,4,6};
+
+    // print_array(arr_main, 4);
+
+    return 0;
+}
